5aac175a63694d8dd4d75b6b0809cd60b9ad91e1
Dmitry Panyushkin <dpanyushkin@es-mdcore.net>
2016-03-25 12:53:44 +0300

    Added Newtonsoft.Json and Jil serializers for benchmarks

--- src/AutoMapper.ExtendedConverters.Benchmarks/Program.cs
+++ src/AutoMapper.ExtendedConverters.Benchmarks/Program.cs
@@ -2,10 +2,17 @@
 
 namespace AutoMapper.ExtendedConverters.Benchmarks
 {
+    using System.IO;
+    using SampleClasses;
+    using Newtonsoft.Json;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var data = Samples.DepartmentsAggregate();
+            File.WriteAllText("benchmark.json", JsonConvert.SerializeObject(data, Formatting.Indented));
+            File.WriteAllText("benchmark.min.json", JsonConvert.SerializeObject(data));
         }
     }
 }
