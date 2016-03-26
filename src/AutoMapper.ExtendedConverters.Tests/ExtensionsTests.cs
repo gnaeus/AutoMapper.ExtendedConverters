36e740b5ec2c1a7e5d60166cd6599be67d38696e
Dmitry Panyushkin <dpanyushkin@expertsolutions.ru>
2016-03-26 18:27:26 +0300

    Fixed ExtensionsTests

--- src/AutoMapper.ExtendedConverters.Tests/ExtensionsTests.cs
+++ src/AutoMapper.ExtendedConverters.Tests/ExtensionsTests.cs
@@ -1,24 +1,33 @@
 ﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AutoMapper.ExtendedConverters.SampleClasses;
 
 namespace AutoMapper.ExtendedConverters.Tests
 {
     [TestClass]
     public class ExtensionsTests
     {
+        class Model
+        {
+            public int Id { get; set; }
+        }
+
+        class Entity
+        {
+            public int Id { get; set; }
+        }
+
         [TestMethod]
         public void ShouldProduceValidConfiguration()
         {
             var config = new MapperConfiguration(cfg => {
 
-                cfg.CreateMap<Customer, Customer>().UsingCompiledConverter();
+                cfg.CreateMap<Model, Entity>().UsingCompiledConverter();
 
-                cfg.CreateMap<List<Customer>, List<Customer>>()
-                    .UsingListConverter(s => s.Id, d => d.Id);
+                cfg.CreateMap<List<Model>, List<Entity>>()
+                    .UsingListConverter(m => m.Id, e => e.Id);
 
-                cfg.CreateMap<IEnumerable<Customer>, List<Customer>>()
-                    .UsingCollectionConverter((Customer s) => s.Id, (Customer d) => d.Id);
+                cfg.CreateMap<IEnumerable<Model>, List<Entity>>()
+                    .UsingCollectionConverter((Model m) => m.Id, (Entity e) => e.Id);
             });
 
             config.AssertConfigurationIsValid();
